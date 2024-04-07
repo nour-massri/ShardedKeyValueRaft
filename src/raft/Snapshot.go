76b6909e774fc49e9320a61595b876f7f14d5a15
Nour Massri <mnmassri@mit.edu>
2024-04-07 16:48:10 -0400

    snapshot

--- src/raft/Snapshot.go
+++ src/raft/Snapshot.go
@@ -1,13 +1,11 @@
 package raft
 
 type InstallSnapshotArgs struct {
-	Term              int    // leader's term
-	LeaderId          int    // so follower can redirect clients
-	LastIncludedIndex int    // the snapshot replaces all entries up through and including this index
-	LastIncludedTerm  int    // term of lastIncludedIndex
-	Data              []byte // raw bytes of the snapshot chunk, starting at offset
-	// Offset         int    // byte offset where chunk is positioned in the snapshot file
-	// Done           bool   // true if this is the last chunk
+	Term int
+	LeaderId int
+	LastIncludedIndex int
+	LastIncludedTerm  int
+	Data []byte
 }
 
 type InstallSnapshotReply struct {
@@ -19,45 +17,46 @@ func (rf *Raft) InstallSnapshot(args *InstallSnapshotArgs, reply *InstallSnapsho
 	defer rf.mu.Unlock()
 	reply.Term = rf.currentTerm
 
-	// 1. reply false if term < currentTerm
-	if args.Term < rf.currentTerm {
-		return
-	}
+
 	if args.Term > rf.currentTerm {
 		rf.ToFollower(args.Term)
 	}
+
+	// 1. Reply immediately if term < currentTerm
+	if args.Term < rf.currentTerm {
+		return
+	}
+
 	send(rf.electionResetCh)
 
-	// check snapshot may expire by lock competition, otherwise rf.logs may overflow below
+	//5. Save snapshot file, discard any existing or partial snapshot
+	//with a smaller index 
+	//in other words discard installsnapshot if smaller index
 	if args.LastIncludedIndex <= rf.lastIncludedIndex {
 		return
 	}
 
-	// 6. if existing log entry has the same index and term as snapshot's last included entry,
-	// retain log entries following it and reply
-	if args.LastIncludedIndex < rf.logLen()-1 {
-		// the args.LastIncludedIndex log has agreed, if there are more logs, just retain them
-		rf.log = append(make([]LogEntry, 0), rf.log[args.LastIncludedIndex-rf.lastIncludedIndex:]...)
+	if args.LastIncludedIndex + 1 < rf.logLen() {
+		// 6. if existing log entry has the same index and term as snapshot's last included entry,
+		// retain log entries following it and reply
+		rf.log = rf.getLogSlice(args.LastIncludedIndex, rf.logLen())
 	} else {
 		// 7. discard the entire log
-		// empty log use for AppendEntries RPC consistency check
 		rf.log = []LogEntry{{args.LastIncludedTerm, nil}}
 	}
 
-	// update snapshot state and persist them
+	// 8. reset state machine using snapshot contents (and load snapshot's cluster configuration)
 	rf.lastIncludedIndex, rf.lastIncludedTerm = args.LastIncludedIndex, args.LastIncludedTerm
-	rf.persist(args.Data)
-
-	// force the follower's log catch up with leader
 	rf.commitIndex = max(rf.commitIndex, rf.lastIncludedIndex)
 	rf.lastApplied = max(rf.lastApplied, rf.lastIncludedIndex)
-	if rf.lastApplied > rf.lastIncludedIndex {
-		return
+	rf.persist(args.Data)
+
+
+	if rf.lastApplied <= rf.lastIncludedIndex {
+		// 8. reset state machine using snapshot contents (and load snapshot's cluster configuration)
+		rf.applyChProxy  <- ApplyMsg{SnapshotValid: true, Snapshot: args.Data, SnapshotIndex: rf.lastIncludedIndex}
 	}
 
-	// 8. reset state machine using snapshot contents (and load snapshot's cluster configuration)
-	applyMsg := ApplyMsg{SnapshotValid: true, Snapshot: args.Data, SnapshotIndex: rf.lastIncludedIndex}
-	rf.applyChProxy <- applyMsg
 }
 
 func (rf *Raft) sendInstallSnapshot(peer int)  {
